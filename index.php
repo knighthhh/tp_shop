46a336ede627ca23d4b482c14b9a6e8596b49fba
knighthhh <775340093@qq.com>
2018-05-07 00:09:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header("content-type:text/html; charset=utf-8");
+define("APP_DEBUG", true);
+
+define('HOME_CSS','/myshop/Home/Public/css/');
+define('HOME_IMG','/myshop/Home/Public/img/');
+
+define('ADMIN_CSS','/myshop/Admin/Public/css/');
+define('ADMIN_IMG','/myshop/Admin/Public/img/');
+
+define('SITE_URL',"");
+
+include ("ThinkPHP/ThinkPHP.php");
