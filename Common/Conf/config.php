f61628bdc404984e498f0b505c950d2327e4472e
knighthhh <775340093@qq.com>
2018-05-07 00:13:16 +0800

    Add files via upload

---
+++ Common/Conf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //显示跟踪信息
+    'SHOW_PAGE_TRACE' => true,
+    //开启smarty模板
+    'TMPL_ENGINE_TYPE' => 'Smarty',
+    
+    //数据库配置
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'testshop',
+    'DB_USER' => '',
+    'DB_PWD' => '',
+    'DB_PREFIX' => 'sw_',
+    'DB_CHARSET' => 'utf8',
+);
